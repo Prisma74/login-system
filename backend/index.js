83046ce964ebd323086566355da61506fdd3d3b6
Prisma74 <solopipe2001@gmail.com>
2025-01-27 10:37:07 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.post("/login", async (req, res) => {
 
   const isPasswordValid = await bcrypt.compare(password, data.password);
   if (!isPasswordValid)
-    return res.status(400).json({ error: "Incorrect password" });
+    return res.status(400).json({ error: "Incorrect passwor" });
 
   const token = jwt.sign(
     { id: data.id, email: data.email },
